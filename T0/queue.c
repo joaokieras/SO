7f0fb9f9783481fa25e9ecbd4006e1131f6e5998
JOAO PEDRO KIERAS OLIVEIRA <jpko19@inf.ufpr.br>
2022-06-15 14:43:32 -0300

    Add comments and finished

--- T0/queue.c
+++ T0/queue.c
@@ -1,20 +1,21 @@
+// GRR20190379 João Pedro Kieras Oliveira
 #include "queue.h"
 #include <stdio.h>
 
 int queue_append(queue_t **queue, queue_t *elem){
     // Testa fila
     if(queue == NULL){
-        fprintf(stderr, "### Error: queue doesn't exists\n");
+        fprintf(stderr, "### Erro: a fila não existe\n");
         return -1;
     }
     // Testa elemento
     if(elem == NULL){
-        fprintf(stderr, "### Error: element doesn't exists\n");
+        fprintf(stderr, "### Erro: o elemento não existe\n");
         return -1;
     }
     // Testa elemento em outra fila
     if(elem->prev != NULL || elem->next != NULL){
-        fprintf(stderr, "### Error: element alredy exists in another queue\n");
+        fprintf(stderr, "### Erro: o elemento já existe em outra fila\n");
         return -1;
     }
     // Fila vazia 
@@ -25,10 +26,10 @@ int queue_append(queue_t **queue, queue_t *elem){
         return 0;
     }
     else{ // Fila não vazia - insere no final 
-        queue_t *last = (*queue)->prev;
-        last->next = elem;
+        queue_t *aux = (*queue)->prev;
+        aux->next = elem;
         elem->next = *queue;
-        elem->prev = last;
+        elem->prev = aux;
         (*queue)->prev = elem;
         return 0;
     }
@@ -37,22 +38,22 @@ int queue_append(queue_t **queue, queue_t *elem){
 
 int queue_remove(queue_t **queue, queue_t *elem){
     if(queue == NULL){
-        fprintf(stderr, "### Error: queue doesn't exists\n");
+        fprintf(stderr, "### Erro: a fila não existe\n");
         return -1;
     }
     // Testa fila vazia
     if(*queue == NULL){
-        fprintf(stderr, "### Error: queue is empty\n");
+        fprintf(stderr, "### Erro: a fila está vazia\n");
         return -1;
     }
 
     if(elem == NULL){
-        fprintf(stderr, "### Error: element doesn't exists\n");
+        fprintf(stderr, "### Erro: o elemento não existe\n");
         return -1;
     }
-
+    // Testa elemento nulo
     if(elem->prev == NULL || elem->next == NULL){
-        fprintf(stderr, "### Error: element doesn't belong to any queue\n");
+        fprintf(stderr, "### Erro: o elemento não pertence a nenhuma fila\n");
         return -1;
     }
 
@@ -62,55 +63,56 @@ int queue_remove(queue_t **queue, queue_t *elem){
     while(!pertence && aux != *queue){
         if(elem == aux){
             pertence = 1;
-            aux = aux->prev;
+            break;
         }
         aux = aux->next;
     } 
-    // "último" elemento, no caso o primeiro da fila, já que a iteração começa em queue->next
-    if(aux == elem)
+    if(aux == elem) // "último" elemento, no caso o primeiro da fila, já que a iteração começa em queue->next
         pertence = 1;
 
     if(!pertence){
-       fprintf(stderr, "### Error: element doesn't belong to the queue\n");
+       fprintf(stderr, "### Erro: o elemento não pertence a fila\n");
         return -1; 
     }  
-    // Único elemento
-    if(queue_size(*queue) == 1)
+    // Fila com um único elemento
+    if(queue_size(*queue) == 1){
         *queue = NULL;
+        elem->prev = NULL;
+        elem->next = NULL;
+        return 0;
+    }
     else{
-        // Primeiro elemento
+        //Caso o elemento seja o primeiro da fila, o próximo será o novo primeiro elem.
         if(*queue == elem)
             *queue = elem->next;
-
         elem->next->prev = elem->prev;
         elem->prev->next = elem->next;
+        elem->prev = NULL;
+        elem->next = NULL;
+        return 0;
     }
-
-    elem->prev = NULL;
-    elem->next = NULL;
-
-    return 0;
+    return -1;
 }
 
-int queue_size (queue_t *queue){
-    int cont = 0;
+int queue_size(queue_t *queue){
     if(queue == NULL)
-        return cont;
+        return 0;
     
+    int cont = 0;
     queue_t *aux = queue->next;
     while(aux != queue){
         cont++;
         aux = aux->next;
     }
+    // Conta o primeiro elemento que não participou da iteração
     cont++;
     return cont;
 }
 
-void queue_print (char *name, queue_t *queue, void print_elem (void*) ){
+void queue_print(char *name, queue_t *queue, void print_elem (void*) ){
     int tam = queue_size(queue);
-    printf("%s [", name);
-    // Fila vazia
-    if(tam == 0){
+    printf("%s: [", name);
+    if(tam == 0){ // Fila vazia
         printf("]\n");
         return;
     }
@@ -123,9 +125,10 @@ void queue_print (char *name, queue_t *queue, void print_elem (void*) ){
         aux = aux->next;
         tam--;
     }
-    // último elemento
+    // "último" elemento
     if(tam != 0)
         print_elem(aux->prev);
 
     printf("]\n");
+    return;
 }
