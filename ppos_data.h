d24fdb9346135ce23a051c57897f9f0ad927019c
joao <jpkieras.oliveira@gmail.com>
2022-06-27 20:50:05 -0300

    Started ppos_core

--- ppos_data.h
+++ ppos_data.h
@@ -7,42 +7,42 @@
 #ifndef __PPOS_DATA__
 #define __PPOS_DATA__
 
-#include <ucontext.h>		// biblioteca POSIX de trocas de contexto
+#include <ucontext.h> // biblioteca POSIX de trocas de contexto
+#define STACKSIZE 64 * 1024
 
 // Estrutura que define um Task Control Block (TCB)
 typedef struct task_t
 {
-  struct task_t *prev, *next ;		// ponteiros para usar em filas
-  int id ;				// identificador da tarefa
-  ucontext_t context ;			// contexto armazenado da tarefa
-  short status ;			// pronta, rodando, suspensa, ...
-  short preemptable ;			// pode ser preemptada?
-   // ... (outros campos serão adicionados mais tarde)
-} task_t ;
+  struct task_t *prev, *next; // ponteiros para usar em filas
+  int id;                     // identificador da tarefa
+  ucontext_t context;         // contexto armazenado da tarefa
+  short status;               // pronta, rodando, suspensa, ...
+  short preemptable;          // pode ser preemptada?
+                              // ... (outros campos serão adicionados mais tarde)
+} task_t;
 
 // estrutura que define um semáforo
 typedef struct
 {
   // preencher quando necessário
-} semaphore_t ;
+} semaphore_t;
 
 // estrutura que define um mutex
 typedef struct
 {
   // preencher quando necessário
-} mutex_t ;
+} mutex_t;
 
 // estrutura que define uma barreira
 typedef struct
 {
   // preencher quando necessário
-} barrier_t ;
+} barrier_t;
 
 // estrutura que define uma fila de mensagens
 typedef struct
 {
   // preencher quando necessário
-} mqueue_t ;
+} mqueue_t;
 
 #endif
-
