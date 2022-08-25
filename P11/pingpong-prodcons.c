e7216c11628f5a4468aa0d7eedebe12544c0df08
João Kieras <jpkieras.oliveira@gmail.com>
2022-08-25 16:33:05 -0300

    Funcionando menos remover da fila

--- P11/pingpong-prodcons.c
+++ P11/pingpong-prodcons.c
@@ -15,6 +15,7 @@ semaphore_t s_item, s_buffer, s_vaga;
 
 void insere_item_buffer(int i);
 int retira_item_buffer();
+void print_fila();
 
 void taskProd(void *arg){
     while(1){
@@ -23,7 +24,7 @@ void taskProd(void *arg){
         sem_down(&s_vaga);
         sem_down(&s_buffer);
         insere_item_buffer(item);
-        printf(" %d\n", item);
+        printf("%s %d\n", (char*) arg, item);
         sem_up(&s_buffer);
         sem_up(&s_item);
     }
@@ -38,7 +39,7 @@ void taskCons(void *arg){
         item_retirado = retira_item_buffer();
         sem_up(&s_buffer);
         sem_up(&s_vaga);
-        printf(" %d\n", item_retirado);
+        printf("%s %d\n", (char*) arg, item_retirado);
         task_sleep(timeSleep);
     }
     task_exit(0);
@@ -60,6 +61,8 @@ int main(int argc, char *argv[]){
     task_create(&c1, taskCons, "                c1 consumiu");
     task_create(&c2, taskCons, "                c2 consumiu");
 
+    task_join(&p3);
+
     // Destrói semáforos
     sem_destroy(&s_item);
     sem_destroy(&s_vaga);
@@ -71,18 +74,30 @@ int main(int argc, char *argv[]){
 }
 
 void insere_item_buffer(int num){
-    int i = 0;
-    for(i = 0;i < TAM_BUFFER - 1;i++){
-        if(queueItems[TAM_BUFFER - 1] == 0){
-            queueItems[i + 1] = queueItems[i];
+    int i;
+    for(i = 0;i < TAM_BUFFER;i++){
+        if(queueItems[i] == 0){
+            queueItems[i] = num;
+            print_fila();
+            return;
         }
-        else
-            return;       
     }
-    queueItems[0] = num;   
+       
     return;
 }
 
 int retira_item_buffer(){
-    return queueItems[TAM_BUFFER - 1];
+    int num = queueItems[0];
+    for(int i = 0;i < TAM_BUFFER - 2;i++){
+        queueItems[i] = queueItems[i + 1];
+    }
+    queueItems[TAM_BUFFER - 1] = 0;
+    return num;
+}
+
+void print_fila(){
+    printf("Fila: ");
+    for(int i = 0;i < TAM_BUFFER;i++)
+        printf("%d ", queueItems[i]);
+    printf("\n");
 }
